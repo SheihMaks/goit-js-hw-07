1212fa183ed21f2b880f89f3f0b17bc593c6bf47
Maksym Padalka <sheihmaks@gmail.com>
2022-04-27 20:45:24 +0300

    +

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryItemsCard = galleryItems
+  .map(({ preview, original, description }) => {
+    galleryContainer.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`
+    );
+  })
+  .join("");
+
+const onOpenInModalCard = (event) => {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  onCreateCardInModal(event.target.getAttribute("data-source"));
+};
+
+let instance = null;
+
+function onCreateCardInModal(link) {
+  instance = basicLightbox.create(`
+    <img src="${link}" class="gallery__image">
+`);
+  instance.show();
+  window.addEventListener("keydown", onCloseModalByEsc);
+}
+
+function onCloseModalByEsc(event) {
+  if (event.code === "Escape") {
+    instance.close();
+    window.removeEventListener("keydown", onCloseModalByEsc);
+  }
+}
+
+galleryContainer.addEventListener("click", onOpenInModalCard);
